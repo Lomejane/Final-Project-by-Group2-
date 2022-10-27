a1ccfbb274cd3c8239de4fa1721c07066db51f72
EnerioBalungcas <enerio.balungcas@gmail.com>
2022-10-27 23:36:02 +0800

    properties view

---
+++ Group2-Last-Project/app/Http/Controllers/PropertiesController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Properties;
+use Illuminate\Support\Facades\DB;
+
+class PropertiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return Properties::all();
+        // $properties = Properties::all();
+        $properties = DB::table("properties_tbl")
+        ->leftJoin("province_tbl", "properties_tbl.property_province_id", "=", "province_tbl.province_id")
+        ->leftJoin("city_municipality_tbl", "properties_tbl.city_mun_id", "=", "city_municipality_tbl.city_mun_id")
+        // ->leftJoin("image_tbl", "property_image_id", "=", "image_tbl.image_id")
+        ->leftJoin("images", "property_image_id", "=", "images.id") 
+        ->orderBy("province_description", "asc")
+        ->get();
+        return view('properties.index')->with('properties', $properties);
+
+        //province_tbl query
+        $province = DB::table("province_tbl")
+        ->distinct('province_description')
+        ->orderBy("province_description", "asc")
+        ->get();
+        return view('properties.index')->with('properties', $properties);
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- Group2-Last-Project/routes/web.php
+++ Group2-Last-Project/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ImageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,8 +18,14 @@ Route::get('about', 'App\Http\Controllers\PagesController@about');
 Route::get('home', 'App\Http\Controllers\PagesController@home');
 Route::get('contact', 'App\Http\Controllers\PagesController@contact');
 Route::get('modalwindow', 'App\Http\Controllers\PagesController@modalwindow');
-Route::get('properties', 'App\Http\Controllers\PagesController@properties');
+// Route::get('properties', 'App\Http\Controllers\PagesController@properties');
 Route::resource('agent', 'App\Http\Controllers\ContactsController');
-Auth::routes();
+Route::resource('properties', 'App\Http\Controllers\PropertiesController');
+Route::resource('province', 'App\Http\Controllers\ProvinceController');
+// Auth::routes();
 
+Route::get('/add-image', [ImageController::class, 'create'])->name('image.add');
+Route::post('/store-image', [ImageController::class, 'store'])->name('image.store');
+Route::get('/show-image', [ImageController::class, 'show'])->name('image.show');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
